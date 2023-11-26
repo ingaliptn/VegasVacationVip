f3b681d23f18fce413d260619fd3adf82644458e
Mykyta Tkachuk <nikitatkachuk6@gmail.com>
2023-11-26 14:29:39 +0200

    Add project

---
+++ VegasVacationVip/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace VegasVacationVip.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Package()
+        {
+            return View();
+        }
+        public IActionResult Hotels()
+        {
+            return View();
+        }
+
+        public IActionResult Gifts() {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+    }
+}
